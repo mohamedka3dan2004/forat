b1c053cfd04faa075e1a1dbfc1ef93abcb9e493b
mohamedka3dan2004 <mohammedkadaan34@gmail.com>
2025-04-08 21:07:36 +0300

    add controller post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\User;
 
 class Post extends Model
 {
@@ -19,13 +20,11 @@ class Post extends Model
         'title',
         'content',
         'user_id',
+        'status',
     ];
 
-    /**
-     * Get the user that owns the post.
-     */
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
-} 
+}
